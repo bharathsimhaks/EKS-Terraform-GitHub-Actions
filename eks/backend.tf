0f8ec7c393e46ee608fd7edcc4f42169d27ae661
bharathsimhaks <126157034+bharathsimhaks@users.noreply.github.com>
2024-12-17 16:57:25 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,4 +1,10 @@
-
+terraform {
+  required_version = "~> 1.9.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.49.0"
+    }
   }
   backend "s3" {
     bucket         = "abhivideocor"
