8f8f1000c3f181b8c79e00593c3046c072a11e73
bharathsimhaks <126157034+bharathsimhaks@users.noreply.github.com>
2024-12-17 16:45:29 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "abhivideocor"
-    region         = "us-east-2"
+    region         = "ap-south-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
